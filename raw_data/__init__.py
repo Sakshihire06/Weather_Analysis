08b2bbdd10104f7fd3d475520288090733dfa918
pareeksra <pareeksra@gmail.com>
2026-03-27 23:37:35 +0530

    __init__.py

--- raw_data/__init__.py
+++ raw_data/__init__.py
@@ -1,1 +1,6 @@
+from .mumbai_raw import get_raw_data as get_mumbai_raw
+from .delhi_raw import get_raw_data as get_delhi_raw
+from .dehradun_raw import get_raw_data as get_dehradun_raw
+from .jodhpur_raw import get_raw_data as get_jodhpur_raw
 
+# or from raw_data import get_mumbai_raw, get_delhi_raw
